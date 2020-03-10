870ca3cf9d298d339d12efe33b6b59094263c990
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-03-10 12:08:27 -0600

    initial commit, navbar

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'Danielle Matton',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: "A title here",
+        subtitle: "Projects that make a different",
+        text: "Checkout my projects below"
+      },
+      about: {
+        title: "about me"
+      },
+      contact: {
+        title: "Let's talk"
+      }
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Danielle Matton</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    );
+  }
+}
+
+export default App;
